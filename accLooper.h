755efc68bd6b6e5a8938762b1f1a96981f1598c1
Alberto Sanchez Hernandez <alberto.sanchez@mail.com>
2015-10-16 08:12:12 -0500

    adding map for err, making limit in histograms, folding in rapitidy

--- accLooper.h
+++ accLooper.h
@@ -18,11 +18,19 @@ class accLooper : public treeLooper {
   virtual void doLoop();
 
   void doAcc();
+  void doAccErr();
 
   TH2D getAcc(){return acc_y_pt_h;};
+  TH2D getAccErr(){return err_acc_y_pt_h;};
+  TH2D getAccAll(){return all_y_pt_h;};
+  TH2D getAccPas(){return pas_y_pt_h;};
 
 private:
 
+  const Int_t nb_y_  =  25;
+  const Int_t nb_pt_ = 100;
+  const Double_t rapmin_ = 0.;
+  const Double_t rapmax_ = 2.5; 
   const bool barrel_;
   TH1D all_pt_h;
   TH1D all_y_h;
@@ -36,6 +44,7 @@ private:
   TH1D acc_y_h;
   TH2D acc_y_pt_h;
 
+  TH2D err_acc_y_pt_h;
 
 };
 
